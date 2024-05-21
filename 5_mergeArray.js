5771cd77b75763e0107984ca5722db7732549eb3
KuntalRathod <nature.kr70@gmail.com>
2024-05-21 18:20:58 +0530

    Refactor array_merge.html to use spread operator for merging arrays

---
+++ 5_mergeArray.js
@@ -0,0 +1,4 @@
+data_1 = [1, 2, 3, 4, 5]
+data_2 = [10, 20, 30, 40, 50, 60]
+newData = [...data_1, ...data_2]
+console.log(newData);
---
+++ 5_reverseArray.js
@@ -0,0 +1,3 @@
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+data.reverse()
+console.log(data);
